39dc270c49145181064c389da231406a475ebe6d
SUBHASH KOLI <subhashkoli.10.2.4@gmail.com>
2026-01-05 15:52:31 +0530

    Implement multiple ggplot visualizations
    Added various ggplot visualizations for mtcars dataset.

--- python-data-analysis/mtcar-dataset.py
+++ python-data-analysis/mtcar-dataset.py
@@ -1,6 +1,22 @@
-
-
 from plotnine import *
 from plotnine.data import mtcars
 
 mtcars.head()
+
+#2-D
+(ggplot(mtcars, aes('wt', 'mpg')) + geom_point() + theme_bw())
+
+#3-D
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)', size='cyl')) + geom_point() + theme_bw())
+
+#4-D
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)')) + geom_point() + facet_wrap('~cyl') + theme_bw())
+
+#visualize data dimensions and some relevant statistics
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)')) + geom_point() + stat_smooth(method='lm') + theme_bw())
+
+#5-D
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)', size='cyl')) + geom_point() + facet_wrap('~am') + theme_bw())
+
+#6-D
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)', size='cyl')) + geom_point() + facet_grid('am ~ carb') + theme_bw())
